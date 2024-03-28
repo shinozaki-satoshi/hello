cbc998906d040e359ae77cd3afdac959b16dd766
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-03-28 14:26:11 +0900

    html

--- src/main/java/com/example/hello/HelloApplicationController.java
+++ src/main/java/com/example/hello/HelloApplicationController.java
@@ -1,13 +1,16 @@
 package com.example.hello;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloApplicationController {
-    @RequestMapping("/")  // ルートへこのメソッドをマップする
-    public String test() {
-        return "Hello World";
+    @RequestMapping(value="/", method = RequestMethod.GET)  // ルートへこのメソッドをマップする
+    public String test(Model model) {
+        model.addAttribute("msg", "Hello World!!!");
+        return "hello";
     }
 
 }
