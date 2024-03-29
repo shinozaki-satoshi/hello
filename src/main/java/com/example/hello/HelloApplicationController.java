3d6ce346e5bd913bae2fbb67711bf53ffac0fbb2
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-03-29 16:40:18 +0900

    CSS

--- src/main/java/com/example/hello/HelloApplicationController.java
+++ src/main/java/com/example/hello/HelloApplicationController.java
@@ -3,7 +3,6 @@ package com.example.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
