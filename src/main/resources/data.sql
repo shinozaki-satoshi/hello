189320f9a7c97dd7c240d31c7ca04bf2bfc6fb84
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-04-10 18:21:15 +0900

    ローカルできたーーーー！！！

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Theme (theme_id, theme_name, deadline_from, deadline_to) VALUES
+(1, 'Spring Cleaning', '2024-04-01', '2024-04-15'),
+(2, 'Summer Vacation Plans', '2024-05-01', '2024-08-31'),
+(3, 'Year-End Review', '2024-11-01', '2024-12-31');
