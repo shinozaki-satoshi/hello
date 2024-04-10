6fa13cba0ced18a0c3267b79133d99139285b6fd
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-04-10 11:50:00 +0900

    s

--- src/main/java/com/example/hello/mapper/ThemeMapper.java
+++ src/main/java/com/example/hello/mapper/ThemeMapper.java
@@ -1,16 +1,25 @@
 package com.example.hello.mapper;
 
-import com.example.hello.entity.Theme;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Mapper
-@Repository
-public interface ThemeMapper {
+import com.example.hello.entity.Theme;
 
-    @Select("SELECT * FROM Theme")
-    List<Theme> findAllThemes();
+@Repository
+public class ThemeMapper {
+
+    private final SqlSessionTemplate sqlSessionTemplate;
+
+    @Autowired
+    public ThemeMapper(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    public List<Theme> findAllThemes() {
+        return sqlSessionTemplate.selectList("com.example.hello.mapper.ThemeMapper.findAllThemes");
+    }
 }
