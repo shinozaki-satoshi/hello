a92a83d05115b775b3dc527535b688baf721118b
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-04-12 14:30:00 +0900

    共通エラーできた！！

---
+++ src/main/java/com/example/hello/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return "error";
+    }
+}
