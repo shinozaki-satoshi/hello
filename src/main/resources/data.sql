5967e06acedadc97649804c3710fb901f6ed72ed
shinozaki-satoshi <shinozakisatoshi0706@gmail.com>
2024-04-12 11:14:34 +0900

    戻るボタンをいい感じに

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Theme (theme_id, theme_name, deadline_from, deadline_to) VALUES
-(1, 'Spring Cleaning', '2024-04-01', '2024-04-15'),
-(2, 'Summer Vacation Plans', '2024-05-01', '2024-08-31'),
-(3, 'Year-End Review', '2024-11-01', '2024-12-31');
+INSERT INTO Theme (theme_name, deadline_from, deadline_to) VALUES
+('こんな法律はマッチョだ', '2024-04-01', '2024-04-15'),
+('スパイファミリーマートでありそうなことは？', '2024-05-01', '2024-08-31'),
+('メンヘラなお医者さん、どんなの？', '2024-11-01', '2024-12-31');
